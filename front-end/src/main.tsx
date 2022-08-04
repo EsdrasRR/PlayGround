a4363497a83cae858f339fd1cbcd275999b19364
André Ramos <andre_ramosdovale@outlook.com>
2022-08-04 14:23:25 -0300

    setup jest, testing library and generic files

--- front-end/src/main.tsx
+++ front-end/src/main.tsx
@@ -2,7 +2,7 @@ import { ChakraProvider } from '@chakra-ui/react'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
+import './assets/styles/styles'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
